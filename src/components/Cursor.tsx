579db2e0150ed9f3dd9aac5bfbd5f3ce09621921
keyurparalkar <keur.plkar@gmail.com>
2024-04-04 16:18:50 +0530

    style: added shape to the cursor

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -13,13 +13,28 @@ const StyledCursor = styled.div<CursorProps>`
 	position: absolute;
 	top: ${(props) => props.y}px;
 	left: ${(props) => props.x}px;
-	background-color: red;
+
+	& span {
+		color: white;
+		background-color: red;
+		padding: 0.3rem;
+		border-radius: 1rem;
+		font-size: 14px;
+		font-weight: bold;
+	}
 `;
 
 const Cursor = (props: CursorProps) => {
 	const { x, y } = props;
 
-	return <StyledCursor id="cursor" x={x} y={y} />;
+	return (
+		<StyledCursor id="cursor" x={x} y={y}>
+			<svg viewBox="0 0 50 50">
+				<polyline points="10,50 0,0 50,25 20,25" fill="red" />
+			</svg>
+			<span id="cursor-text">User1</span>
+		</StyledCursor>
+	);
 };
 
 export default Cursor;
