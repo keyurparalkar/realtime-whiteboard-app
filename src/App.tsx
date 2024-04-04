8e1d3e5dbaf86b11710dfe72906181c0826b8032
keyurparalkar <keur.plkar@gmail.com>
2024-04-04 12:54:48 +0530

    chore: init repository

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import BoardExample from "./Board";
+
+function App() {
+	return (
+		<>
+			<h1>Live Cursor Example</h1>
+			<BoardExample />
+		</>
+	);
+}
+
+export default App;
