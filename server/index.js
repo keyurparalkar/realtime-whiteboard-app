51a320119f121fe46175da09a04c9496f2df665f
keyurparalkar <keur.plkar@gmail.com>
2024-04-24 15:10:23 +0000

    feat: added socket.io and initialzed onConnection event handler

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import { createServer } from "node:http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
 	res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", (socket) => {
+	console.log("A user connected with id - ", socket.id);
 });
 
 server.listen(3000, () => {
