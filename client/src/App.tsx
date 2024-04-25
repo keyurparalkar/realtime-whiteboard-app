89e7bf6a694c6c8d5fe12c8317fabb24bff5ebdc
keyurparalkar <keur.plkar@gmail.com>
2024-04-25 16:12:01 +0530

    feat: enabled video sharing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
-import { useEffect } from "react";
+import { ElementRef, useEffect, useRef } from "react";
 import "./App.css";
 import { io } from "socket.io-client";
 
+/**
+ * Program flow:
+ * 1. On page load, permission should be asked to access the video camera.
+ * 2. Once given, the video element should be shown in the component.
+ */
+
 function App() {
+	const videoRef = useRef<ElementRef<"video">>(null);
+
 	useEffect(() => {
 		const socket = io("http://localhost:3000");
 
@@ -10,10 +18,32 @@ function App() {
 			console.log({ id, count });
 		});
 	}, []);
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const mediaStream = await navigator.mediaDevices.getUserMedia({
+					video: {
+						width: 400,
+						height: 300,
+					},
+				});
+
+				const video = videoRef.current;
+				if (video) {
+					video.srcObject = mediaStream;
+					video.play();
+				}
+			} catch (err) {
+				console.log({ err });
+			}
+		})();
+	}, []);
+
 	return (
 		<>
 			<h1>Live Cursor Example</h1>
-			<button>start</button>
+			<video width={400} height={300} ref={videoRef} />
 		</>
 	);
 }
