73cc54cda16e62c8308a3860968a029801aa462d
keyurparalkar <keur.plkar@gmail.com>
2024-04-25 10:18:46 +0530

    feat: enabled socket.io and connection with the socket server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
-import BoardExample from "./Board";
-import Peer from "peerjs";
+import { io } from "socket.io-client";
 
 function App() {
-	const [id, setId] = useState<string | null>(null);
-	const [peerCon, setPeerCon] = useState(null);
-
-	// useEffect(() => {
-	// 	const peer = new Peer();
-	// }, [])
-
-	const handleClick = async () => {
-		const peer = new Peer();
-		const id = await peer.id;
-		if (peer.id) {
-			setId(peer.id);
-		} else {
-			setId("no id");
-		}
-	};
-
+	useEffect(() => {
+		const socket = io("http://localhost:3000");
+		console.log({ socket });
+	}, []);
 	return (
 		<>
 			<h1>Live Cursor Example</h1>
-			<span>id: {id}</span>
-			<button onClick={handleClick}>start</button>
+			<button>start</button>
 		</>
 	);
 }
