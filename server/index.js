e20f04c61dadf443064aa67cb8959ccb8e313360
keyurparalkar <keur.plkar@gmail.com>
2024-04-25 10:19:29 +0530

    feat: enabled socket.io connection with cors

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+	cors: {
+		origin: "http://localhost:5173",
+	},
+});
 
 app.get("/", (req, res) => {
 	res.send("<h1>Hello world</h1>");
