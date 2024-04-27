9a25788e9ca047e6015dd9a8ff600358067c69a7
keyurparalkar <keur.plkar@gmail.com>
2024-04-27 22:53:09 +0530

    feat: init new arch for multi peer connections

--- server/index.js
+++ server/index.js
@@ -10,24 +10,38 @@ const io = new Server(server, {
 	},
 });
 
-/**
- * Algorithm would be as follows:
- * 1. When a new client joins we broadcast an event to all connected clients,
- *    stating the id, numberOfClientsConnected and socketId of that client.
- * 2. We create a relay, such that it by passes the message to all the clients except for itself.
- *
- */
+// /**
+//  * Algorithm would be as follows:
+//  * 1. When a new client joins we broadcast an event to all connected clients,
+//  *    stating the id, numberOfClientsConnected and socketId of that client.
+//  * 2. We create a relay, such that it by passes the message to all the clients except for itself.
+//  *
+//  */
+// io.on("connection", (socket) => {
+// 	const clientCount = io.engine.clientsCount;
+// 	const clientIds = Array.from(io.sockets.sockets, (value) => value[0]);
+// 	console.log("A user connected with id - ", socket.id, clientIds);
+
+// 	io.emit("new-user", socket.id, clientCount, clientIds);
+
+// 	socket.on("init", (toId, msg) => {
+// 		console.log({ toId, socketId: socket.id, msg: Object.keys(msg) });
+// 		socket.broadcast.emit("init", toId, msg);
+// 		// io.to(toId).emit("init", socket.id, msg);
+// 	});
+// });
+
 io.on("connection", (socket) => {
-	const clientCount = io.engine.clientsCount;
-	const clientIds = Array.from(io.sockets.sockets, (value) => value[0]);
-	console.log("A user connected with id - ", socket.id, clientIds);
+	const connectedSocketIds = Array.from(
+		io.sockets.sockets,
+		(value) => value[0]
+	);
+	console.log(`New user joind - `, socket.id, connectedSocketIds);
 
-	io.emit("new-user", socket.id, clientCount, clientIds);
+	io.emit("new-user", connectedSocketIds);
 
-	socket.on("init", (toId, msg) => {
-		console.log({ toId, socketId: socket.id, msg: Object.keys(msg) });
-		socket.broadcast.emit("init", toId, msg);
-		// io.to(toId).emit("init", socket.id, msg);
+	socket.on("client-offers", (offers) => {
+		console.log(`Offers from ${socket.id} ${JSON.stringify(offers)}`);
 	});
 });
 
