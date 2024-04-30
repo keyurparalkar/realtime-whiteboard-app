c42fc09a61bff7cfbf6557553b5f1ad5cc8ac0e7
keyurparalkar <keur.plkar@gmail.com>
2024-04-30 16:29:41 +0530

    feat: store ids of new client

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import "./App.css";
 import { createClient } from "@supabase/supabase-js";
+import { nanoid } from "nanoid";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+
+type Payload = {
+	clientId: string;
+	x: number;
+	y: number;
+};
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_PROJECT_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const CURRENT_CLIENT_ID = nanoid();
+
+const clientA = createClient(SUPABASE_URL, SUPABASE_KEY);
+const channel = clientA.channel("room-1");
 
 const throttle = <ArgType,>(
 	instance: unknown,
-	func: (args: ArgType) => unknown
+	func: (args: ArgType) => unknown,
+	delay: number = 2000
 ) => {
 	let flag: NodeJS.Timeout | null = null;
 	const _this = instance;
@@ -13,51 +30,66 @@ const throttle = <ArgType,>(
 			func.call(_this, args);
 			flag = setTimeout(() => {
 				flag = null;
-			}, 2000);
+			}, delay);
 		}
 	};
 };
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_PROJECT_URL;
-const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-// Initialize the JS client
-const clientA = createClient(SUPABASE_URL, SUPABASE_KEY);
-const channel = clientA.channel("room-1");
-
-// Create a function to handle inserts
-const logger = (payload: unknown) => {
-	console.log("Change received! = ", payload);
-};
-
-channel
-	.on("broadcast", { event: "test" }, (payload: unknown) => logger(payload))
-	.subscribe((status) => {
-		if (status !== "SUBSCRIBED") {
-			return null;
-		}
-
-		// Send a message once the client is subscribed
-		channel.send({
-			type: "broadcast",
-			event: "test",
-			payload: { message: "hello, world" },
-		});
-	});
-
 const func = throttle(channel, channel.send);
+
 function App() {
-	const handleClick = () => {
+	const [newClients, setNewClients] = useState<
+		Record<string, { x: number; y: number }>
+	>({});
+	const isFirstRender = useRef(true);
+
+	const logger = (payload: Payload) => {
+		const { clientId, x, y } = payload;
+
+		setNewClients((preVal) => ({
+			...preVal,
+			[clientId]: {
+				x,
+				y,
+			},
+		}));
+	};
+
+	const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
 		func({
 			type: "broadcast",
 			event: "test",
-			payload: { message: "hello, world" },
+			payload: {
+				clientId: CURRENT_CLIENT_ID,
+				x: event.clientX,
+				y: event.clientY,
+			},
 		});
 	};
 
+	useEffect(() => {
+		if (isFirstRender.current) {
+			channel
+				.on(
+					"broadcast",
+					{ event: "test" },
+					(log: { type: "broadcast"; event: string; payload: Payload }) =>
+						logger(log.payload)
+				)
+				.subscribe((status) => {
+					if (status !== "SUBSCRIBED") {
+						return null;
+					}
+				});
+
+			isFirstRender.current = false;
+		}
+	}, []);
+
 	return (
-		<div id="container" onClick={handleClick}>
+		<div id="container" onMouseMove={handleClick}>
 			<h1>Live Cursor Example</h1>
+			<span>{JSON.stringify(newClients, null, 2)}</span>
 		</div>
 	);
 }
