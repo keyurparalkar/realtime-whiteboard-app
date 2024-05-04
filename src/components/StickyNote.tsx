32b4a469b1a01140257a035c774be2b1d8cad990
keyurparalkar <keur.plkar@gmail.com>
2024-05-04 15:58:22 +0530

    feat: enabled note movement

--- src/components/StickyNote.tsx
+++ src/components/StickyNote.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 
 type StickyNoteProps = {
@@ -17,18 +17,45 @@ const StyledNote = styled.div<Omit<StickyNoteProps, "$noteText">>`
 	color: black;
 	font-weight: 500;
 	font-size: 2rem;
+	transform: translate(-150px, -100px);
 `;
 
 const StickyNote = (props: StickyNoteProps) => {
-	const { $noteText, ...rest } = props;
+	const { $noteText, $x, $y } = props;
 	const [text, setText] = useState($noteText || "");
+	const [x, setX] = useState($x || 0);
+	const [y, setY] = useState($y || 0);
+	const isMouseDown = useRef(false);
 
 	const handleInput = (event: React.FormEvent<HTMLDivElement>) => {
 		setText(event.currentTarget.textContent || "");
 	};
 
+	const handleMouseDown = () => {
+		isMouseDown.current = true;
+	};
+
+	const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+		if (isMouseDown.current) {
+			setX(event.clientX);
+			setY(event.clientY);
+		}
+	};
+
+	const handleMouseUp = () => {
+		isMouseDown.current = false;
+	};
+
 	return (
-		<StyledNote id="sticky-note" onInput={handleInput} {...rest}>
+		<StyledNote
+			id="sticky-note"
+			$x={x}
+			$y={y}
+			onInput={handleInput}
+			onMouseDown={handleMouseDown}
+			onMouseMove={handleMouseMove}
+			onMouseUp={handleMouseUp}
+		>
 			{text}
 		</StyledNote>
 	);
